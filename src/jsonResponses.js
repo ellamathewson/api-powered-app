29483561e3ba92a71c469fb45bb482b132be5f6f
Ella Mathewson <ellaofohio@gmail.com>
2019-10-11 13:19:22 -0400

    Fixed issue with displaying faveDrinks submitted by user

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -4,7 +4,7 @@
 
 // Descriptions of AllDrinks from https://github.com/CoffeeJson/json/blob/gh-pages/coffee.json, by Robert James Gabriel
 
-const faveDrinks = {};
+const faveDrinks = [];
 // const today = new Date();
 const allDrinks = require('./allDrinks.json');
 
@@ -53,37 +53,72 @@ const addDrink = (request, response, body) => {
     return respondJSON(request, response, 400, responseJSON);
   }
 
+  // assumes we are doing a creation
   let responseCode = 201;
 
-  if (faveDrinks[body.name]) {
-    if (faveDrinks[body.name].name === body.name
-      && faveDrinks[body.name].date === body.date
-      && faveDrinks[body.name].cafe === body.cafe) {
+  // checks to see if we need to update instead by seeing if the drink already exists
+  let drinkIndex = -1;
+  for (let i = 0; i < faveDrinks.length; i++) {
+    if (faveDrinks[i].name === body.name && faveDrinks[i].cafe === body.cafe) {
       responseCode = 204;
+      drinkIndex = i;
+      break;
     }
-  } else {
-    faveDrinks[body.name] = {};
   }
 
-  console.dir('update');
-  faveDrinks[body.name].name = [];
-  faveDrinks[body.name].cafe = [];
-  faveDrinks[body.name].date = [];
-  faveDrinks[body.name].description = [];
-  faveDrinks[body.name].cost = [];
-  faveDrinks[body.name].rating = [];
+  if (responseCode === 204) {
+    faveDrinks[drinkIndex].name = body.name;
+    faveDrinks[drinkIndex].date = body.date;
+    faveDrinks[drinkIndex].cafe = body.cafe;
+    faveDrinks[drinkIndex].description = body.description;
+    faveDrinks[drinkIndex].cost = body.cost;
+    faveDrinks[drinkIndex].rating = body.rating;
 
-  faveDrinks[body.name].name.push(body.name);
-  faveDrinks[body.name].cafe.push(body.cafe);
-  faveDrinks[body.name].date.push(body.date);
-  faveDrinks[body.name].description.push(body.description);
-  faveDrinks[body.name].cost.push(body.cost);
-  faveDrinks[body.name].rating.push(body.rating);
+    return respondJSONMeta(request, response, responseCode);
+  }
+  // otherwise create a new drink in the array
+  console.dir('pushing new object');
+  faveDrinks.push({
+    name: body.name,
+    date: body.date,
+    cafe: body.cafe,
+    description: body.description,
+    cost: body.cost,
+    rating: body.rating,
+  });
 
-  if (responseCode === 201) {
-    responseJSON.message = 'Added to favorite drinks!';
-    return respondJSON(request, response, responseCode, responseJSON);
-  } return respondJSONMeta(request, response, responseCode);
+  responseJSON.message = 'Added to favorite drinks!';
+  return respondJSON(request, response, responseCode, responseJSON);
+
+  // if (faveDrinks[body.name]) {
+  //   if (faveDrinks[body.name].name === body.name
+  //     && faveDrinks[body.name].date === body.date
+  //     && faveDrinks[body.name].cafe === body.cafe) {
+  //     responseCode = 204;
+  //   }
+  // } else {
+  //   faveDrinks[body.name] = {};
+  // }
+
+  // if drink already exists, edit current one
+  // faveDrinks[body.name].name = [];
+  // faveDrinks[body.name].cafe = [];
+  // faveDrinks[body.name].date = [];
+  // faveDrinks[body.name].description = [];
+  // faveDrinks[body.name].cost = [];
+  // faveDrinks[body.name].rating = [];
+
+  // faveDrinks[body.name].name.push(body.name);
+  // faveDrinks[body.name].cafe.push(body.cafe);
+  // faveDrinks[body.name].date.push(body.date);
+  // faveDrinks[body.name].description.push(body.description);
+  // faveDrinks[body.name].cost.push(body.cost);
+  // faveDrinks[body.name].rating.push(body.rating);
+
+  // if (responseCode === 201) {
+  //   responseJSON.message = 'Added to favorite drinks!';
+  //   return respondJSON(request, response, responseCode, responseJSON);
+  // } return respondJSONMeta(request, response, responseCode);
 };
 
 const notFound = (request, response) => {
