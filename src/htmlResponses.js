1d6abb4a09fb40d8919cb87e491a1391e0425fe1
Ella Mathewson <ellaofohio@gmail.com>
2019-10-12 20:18:13 -0400

    Cleaned up code and added comments

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -2,48 +2,29 @@
 const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
-// const addDrink = fs.readFileSync(`${__dirname}/../client/addDrink.html`);
 
 const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 const beansImage = fs.readFileSync(`${__dirname}/../images/beans.jpg`);
 
+// gets the index page
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(index);
   response.end();
 };
 
-// const getAddDrink = (request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'text/html' });
-//   response.write(addDrink);
-//   response.end();
-// };
-
+// applies css to page
 const getCSS = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/css' });
   response.write(css);
   response.end();
 };
 
+// grabs the image used in the background
 const getImage = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'image/jpeg' });
   response.write(beansImage);
   response.end();
-  // fs.stat(file, (err, status) => {
-  //     if(err) {
-  //         if(err.code === 'ENOENT') {
-  //             response.writeHead(404);
-  //         }
-  //         return response.end(err);
-  //     }
-  //     const stream = writeHead(request, response, stats, file, 'image/jpeg');
-  //     stream.on('open', () => {
-  //         stream.pipe(response);
-  //     });
-  //     stream.on('error', (streamErr) => {
-  //         response.end(streamErr);
-  //     });
-  // })
 };
 
 module.exports = {
